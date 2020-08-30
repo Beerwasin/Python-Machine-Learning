6f6bed58bfaccad9f95a2cc80c94a247e9ba104e
Beerwasin <70392795+Beerwasin@users.noreply.github.com>
2020-08-30 20:19:25 +0700

    Add files via upload

---
+++ KNN/MLModel.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+rng=np.random
+
+#การจำลองข้อมูล
+x=rng.rand(50)*10              #rand  สุ่มตัวเลขค่าบวก
+y=2*x+rng.randn(50)            #randn สุ่มตัวเลขค่าลบ
+
+#linear regression model
+model=LinearRegression()
+x_new=x.reshape(-1,1)
+
+#train
+model.fit(x_new,y)
+
+#test model
+xfit=np.linspace(-1,11)
+xfit_new=xfit.reshape(-1,1)
+
+yfit=model.predict(xfit_new)
+
+
+#analysis model
+plt.scatter(x,y)
+plt.plot(xfit,yfit)             #สร้างเส้นLinearRegression
+plt.show()
+
+#y=ax+c
